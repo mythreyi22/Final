f82404236dfe1e96ddc22d132d795897d16a931b
Steve Borho <steve@borho.org>
2015-04-07 14:40:42 -0400

    ignore new QGSize warning in regression tests

--- utils.py
+++ utils.py
@@ -333,6 +333,7 @@ ignored_x265_warnings = (
     '!! HEVC Range Extension specifications are not finalized !!',
     '!! This output bitstream may not be compliant with the final spec !!',
     'Max TU size should be less than or equal to max CU size, setting max TU size = 16',
+    'QGSize should be less than or equal to maxCUSize, setting QGSize = 16',
     'No thread pool allocated, --wpp disabled',
     'No thread pool allocated, --pme disabled',
     'Support for interlaced video is experimental',
